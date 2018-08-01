d15f4e06342633639f4596783889f43dc51aeb58
Alexander Chermyanin <flamencist@mail.ru>
2018-08-02 00:01:37 +0300

    add fake eventloginstaller and performancecounterinstaller

---
+++ System.Configuration.Install/System.Diagnostics/EventLogInstaller.cs
@@ -0,0 +1,107 @@
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Runtime.InteropServices;
+
+namespace System.Diagnostics
+{
+	/// <summary>Allows you to install and configure an event log that your application reads from or writes to when running. </summary>
+	public class EventLogInstaller : ComponentInstaller
+	{
+		private readonly EventSourceCreationData _sourceData = new EventSourceCreationData(null, null);
+
+		private UninstallAction _uninstallAction;
+
+		/// <summary>Gets or sets the path of the resource file that contains category strings for the source.</summary>
+		/// <returns>The path of the category resource file. The default is an empty string ("").</returns>
+		[ResDescription("Desc_CategoryResourceFile")]
+		public string CategoryResourceFile
+		{
+			get => _sourceData.CategoryResourceFile;
+			set => _sourceData.CategoryResourceFile = value;
+		}
+
+		/// <summary>Gets or sets the number of categories in the category resource file.</summary>
+		/// <returns>The number of categories in the category resource file. The default value is zero.</returns>
+		[ResDescription("Desc_CategoryCount")]
+		public int CategoryCount
+		{
+			get => _sourceData.CategoryCount;
+			set => _sourceData.CategoryCount = value;
+		}
+
+		/// <summary>Gets or sets the name of the log to set the source to.</summary>
+		/// <returns>The name of the log. This can be Application, System, or a custom log name. The default is an empty string ("").</returns>
+		[ResDescription("Desc_Log")]
+		public string Log
+		{
+			get => _sourceData.LogName;
+			set => _sourceData.LogName = value;
+		}
+
+		/// <summary>Gets or sets the path of the resource file that contains message formatting strings for the source.</summary>
+		/// <returns>The path of the message resource file. The default is an empty string ("").</returns>
+		[ResDescription("Desc_MessageResourceFile")]
+		public string MessageResourceFile
+		{
+			get => _sourceData.MessageResourceFile;
+			set => _sourceData.MessageResourceFile = value;
+		}
+
+		/// <summary>Gets or sets the path of the resource file that contains message parameter strings for the source.</summary>
+		/// <returns>The path of the message parameter resource file. The default is an empty string ("").</returns>
+		[ResDescription("Desc_ParameterResourceFile")]
+		public string ParameterResourceFile
+		{
+			get => _sourceData.ParameterResourceFile;
+			set => _sourceData.ParameterResourceFile = value;
+		}
+
+		/// <summary>Gets or sets the source name to register with the log.</summary>
+		/// <returns>The name to register with the event log as a source of entries. The default is an empty string ("").</returns>
+		[ResDescription("Desc_Source")]
+		public string Source
+		{
+			get => _sourceData.Source;
+			set => _sourceData.Source = value;
+		}
+
+		/// <summary>Gets or sets a value that indicates whether the Installutil.exe (Installer Tool) should remove the event log or leave it in its installed state at uninstall time.</summary>
+		/// <returns>One of the <see cref="T:System.Configuration.Install.UninstallAction" /> values that indicates what state to leave the event log in when the <see cref="T:System.Diagnostics.EventLog" /> is uninstalled. The default is Remove.</returns>
+		/// <exception cref="T:System.ComponentModel.InvalidEnumArgumentException">
+		///   <see cref="P:System.Diagnostics.EventLogInstaller.UninstallAction" /> contains an invalid value. The only valid values for this property are Remove and NoAction.</exception>
+		[DefaultValue(UninstallAction.Remove)]
+		[ResDescription("Desc_UninstallAction")]
+		public UninstallAction UninstallAction
+		{
+			get => _uninstallAction;
+			set
+			{
+				if (!Enum.IsDefined(typeof(UninstallAction), value))
+				{
+					throw new InvalidEnumArgumentException(nameof(value), (int)value, typeof(UninstallAction));
+				}
+				_uninstallAction = value;
+			}
+		}
+
+		/// <summary>Copies the property values of an <see cref="T:System.Diagnostics.EventLog" /> component that are required at installation time for an event log.</summary>
+		/// <param name="component">An <see cref="T:System.ComponentModel.IComponent" /> to use as a template for the <see cref="T:System.Diagnostics.EventLogInstaller" />. </param>
+		public override void CopyFromComponent(IComponent component)
+		{
+		}
+
+		/// <summary>Determines whether an installer and another specified installer refer to the same source.</summary>
+		/// <returns>true if this installer and the installer specified by the <paramref name="otherInstaller" /> parameter would install or uninstall the same source; otherwise, false.</returns>
+		/// <param name="otherInstaller">The installer to compare. </param>
+		public override bool IsEquivalentInstaller(ComponentInstaller otherInstaller)
+		{
+			EventLogInstaller eventLogInstaller = otherInstaller as EventLogInstaller;
+			if (eventLogInstaller == null)
+			{
+				return false;
+			}
+			return eventLogInstaller.Source == Source;
+		}
+	}
+}
---
+++ System.Configuration.Install/System.Diagnostics/PerformanceCounterInstaller.cs
@@ -0,0 +1,137 @@
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace System.Diagnostics
+{
+	/// <summary>Specifies an installer for the <see cref="T:System.Diagnostics.PerformanceCounter" /> component.</summary>
+	public class PerformanceCounterInstaller : ComponentInstaller
+	{
+		private string _categoryName = string.Empty;
+
+		private readonly CounterCreationDataCollection _counters = new CounterCreationDataCollection();
+
+		private string _categoryHelp = string.Empty;
+
+		private UninstallAction _uninstallAction;
+
+		private PerformanceCounterCategoryType _categoryType = PerformanceCounterCategoryType.Unknown;
+
+		/// <summary>Gets or sets the performance category name for the performance counter.</summary>
+		/// <returns>The performance category name for the performance counter.</returns>
+		/// <exception cref="T:System.ArgumentNullException">The value is set to null. </exception>
+		/// <exception cref="T:System.ArgumentException">The value is not a valid category name.</exception>
+		[DefaultValue("")]
+		[ResDescription("PCCategoryName")]
+		public string CategoryName
+		{
+			get => _categoryName;
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException(nameof(value));
+				}
+				CheckValidCategory(value);
+				_categoryName = value;
+			}
+		}
+
+		/// <summary>Gets or sets the descriptive message for the performance counter.</summary>
+		/// <returns>The descriptive message for the performance counter.</returns>
+		/// <exception cref="T:System.ArgumentNullException">The value is set to null. </exception>
+		[DefaultValue("")]
+		[ResDescription("PCI_CategoryHelp")]
+		public string CategoryHelp
+		{
+			get => _categoryHelp;
+			set => _categoryHelp = value ?? throw new ArgumentNullException(nameof(value));
+		}
+
+		/// <summary>Gets or sets the performance counter category type.</summary>
+		/// <returns>One of the <see cref="T:System.Diagnostics.PerformanceCounterCategoryType" /> values. </returns>
+		/// <exception cref="T:System.ComponentModel.InvalidEnumArgumentException">The value is not a <see cref="T:System.Diagnostics.PerformanceCounterCategoryType" />.</exception>
+		[DefaultValue(PerformanceCounterCategoryType.Unknown)]
+		[ResDescription("PCI_IsMultiInstance")]
+		public PerformanceCounterCategoryType CategoryType
+		{
+			get => _categoryType;
+			set
+			{
+				if (value < PerformanceCounterCategoryType.Unknown || value > PerformanceCounterCategoryType.MultiInstance)
+				{
+					throw new InvalidEnumArgumentException(nameof(value), (int)value, typeof(PerformanceCounterCategoryType));
+				}
+				_categoryType = value;
+			}
+		}
+
+		/// <summary>Gets a collection of data that pertains to the counters to install.</summary>
+		/// <returns>A <see cref="T:System.Diagnostics.CounterCreationDataCollection" /> that contains the names, help messages, and types of the counters to install.</returns>
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+		[ResDescription("PCI_Counters")]
+		public CounterCreationDataCollection Counters => _counters;
+
+		/// <summary>Gets a value that indicates whether the performance counter should be removed at uninstall time.</summary>
+		/// <returns>One of the <see cref="T:System.Configuration.Install.UninstallAction" /> values. The default is Remove.</returns>
+		/// <exception cref="T:System.ComponentModel.InvalidEnumArgumentException">The value is not an <see cref="T:System.Configuration.Install.UninstallAction" />.</exception>
+		[DefaultValue(UninstallAction.Remove)]
+		[ResDescription("PCI_UninstallAction")]
+		public UninstallAction UninstallAction
+		{
+			get => _uninstallAction;
+			set
+			{
+				if (!Enum.IsDefined(typeof(UninstallAction), value))
+				{
+					throw new InvalidEnumArgumentException(nameof(value), (int)value, typeof(UninstallAction));
+				}
+				_uninstallAction = value;
+			}
+		}
+
+		/// <summary>Copies all the properties from the specified component that are required at install time for a performance counter.</summary>
+		/// <param name="component">The component to copy from. </param>
+		/// <exception cref="T:System.ArgumentException">The specified component is not a <see cref="T:System.Diagnostics.PerformanceCounter" />.-or- The specified <see cref="T:System.Diagnostics.PerformanceCounter" /> is incomplete.-or- Multiple counters in different categories are trying to be installed. </exception>
+		public override void CopyFromComponent(IComponent component)
+		{
+			
+		}
+
+		private static void CheckValidCategory(string categoryName)
+		{
+			if (categoryName == null)
+			{
+				throw new ArgumentNullException(nameof(categoryName));
+			}
+			if (!CheckValidId(categoryName))
+			{
+				throw new ArgumentException(Res.GetString("PerfInvalidCategoryName", 1, 253));
+			}
+		}
+
+		private static bool CheckValidId(string id)
+		{
+			if (id.Length == 0 || id.Length > 253)
+			{
+				return false;
+			}
+			for (int i = 0; i < id.Length; i++)
+			{
+				char c = id[i];
+				if ((i == 0 || i == id.Length - 1) && c == ' ')
+				{
+					return false;
+				}
+				if (c == '"')
+				{
+					return false;
+				}
+				if (char.IsControl(c))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
