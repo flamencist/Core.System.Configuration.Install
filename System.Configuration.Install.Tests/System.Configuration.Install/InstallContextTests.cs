e97b438377e7eb2896e89d70e43f9c6d1f6e9ca1
Chris Benard <chris@chrisbenard.net>
2020-10-26 19:41:43 -0500

    Fixed IsParameterTrue() for /params (matches Framework behavior)

--- System.Configuration.Install.Tests/System.Configuration.Install/InstallContextTests.cs
+++ System.Configuration.Install.Tests/System.Configuration.Install/InstallContextTests.cs
@@ -12,5 +12,18 @@ namespace System.Configuration.Install.Tests.System.Configuration.Install
             Assert.AreEqual("/var/log/log.log", installContext.Parameters["logFile"]);
             Assert.AreEqual("true", installContext.Parameters["LogToConsole"]);
         }
+
+        [TestMethod]
+        public void Should_Return_Parameter_True_For_Parameters()
+        {
+            var installContext = new InstallContext("/var/log/log.log", new[] { "/whatever", "/i", "-debug" });
+
+            Assert.IsTrue(installContext.IsParameterTrue("debug"));
+            Assert.IsTrue(installContext.IsParameterTrue("Debug"));
+            Assert.IsTrue(installContext.IsParameterTrue("i"));
+            Assert.IsTrue(installContext.IsParameterTrue("I"));
+            Assert.IsTrue(installContext.IsParameterTrue("whatever"));
+            Assert.IsTrue(installContext.IsParameterTrue("Whatever"));
+        }
     }
 }
